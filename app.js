c2c56b3fc3f9b760a73a603ce6e44a6ceb7b9727
kaller01 <martin@kallers.se>
2021-02-14 20:11:09 +0100

    Less manual input

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const nodes = {
+//These nodes are from the following video
+//https://www.youtube.com/watch?v=pVfj6mxhdMw
+let someSetOfNodes = {
   A: [
     { node: "B", distance: 6 },
     { node: "D", distance: 1 },
@@ -24,38 +26,77 @@ const nodes = {
   ],
 };
 
-const table = {
-  A: {
-    distance: 0,
-  },
-  B: {
-    distance: 100,
-  },
-  C: {
-    distance: 100,
-  },
-  D: {
-    distance: 100,
-  },
-  E: {
-    distance: 100,
-  },
+//Assignment we have
+//https://d2uusema5elisf.cloudfront.net/books/javascript-algorithms/images/26-dijkstra/graph.png
+let HomeToOffice = {
+  Home: [
+    { node: "A", distance: 5 },
+    { node: "D", distance: 8 },
+  ],
+  A: [
+    { node: "Home", distance: 5 },
+    { node: "C", distance: 3 },
+    { node: "B", distance: 9 },
+  ],
+  B: [
+    { node: "A", distance: 9 },
+    { node: "C", distance: 5 },
+    { node: "Office", distance: 7 },
+  ],
+  C: [
+    { node: "A", distance: 3 },
+    { node: "B", distance: 5 },
+    { node: "D", distance: 4 },
+    { node: "E", distance: 2 },
+  ],
+  D: [
+    { node: "Home", distance: 8 },
+    { node: "C", distance: 4 },
+    { node: "E", distance: 6 },
+  ],
+  E: [
+    { node: "C", distance: 2 },
+    { node: "D", distance: 6 },
+    { node: "Office", distance: 4 },
+  ],
+  Office: [
+    { node: "B", distance: 7 },
+    { node: "E", distance: 4 },
+  ],
 };
 
-const unvisisted = {
-  A: true,
-  B: true,
-  C: true,
-  D: true,
-  E: true,
-};
-
+const startNode = "Home";
+const endNode = "Office";
+const nodes = HomeToOffice;
+const table = constructTable(startNode);
+const unvisisted = constructUnvisited();
 let leftToVisist = Object.keys(unvisisted).length;
-
 let current = {
   distance: 1000,
   node: "",
 };
+
+while (unvisisted[endNode]) {
+  current = getShortestPath();
+  explore(current.node);
+}
+
+function constructTable(start) {
+  let table = {};
+  for (const node in nodes) {
+    if (node == start) table[node] = { distance: 0 };
+    else table[node] = { distance: 100 };
+  }
+  return table;
+}
+
+function constructUnvisited() {
+  let unvisisted = {};
+  for (node in nodes) {
+    unvisisted[node] = true;
+  }
+  return unvisisted;
+}
 
 function getShortestPath() {
   for (const node in unvisisted) {
@@ -87,9 +128,4 @@ function explore(node) {
   };
   console.log("Current table:");
   console.log(table);
-}
-
-while (leftToVisist > 0) {
-  current = getShortestPath();
-  explore(current.node);
 }
